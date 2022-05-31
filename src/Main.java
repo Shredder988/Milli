afa1d5e68704a09427e1a1ae99e1608c2e81c32e
Shredder988 <dimankolevatov@yandex.ru>
2022-05-31 22:37:50 +0300

    third commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        long amount = 5000; // стоимость билета
-        long bonusMile = 20; // стоимость одной бонусной милли в рублях
-        long numberMile = amount / bonusMile; //расчет количества бонусных милль
-        System.out.println("Сколько боннусных милль мы получим, если:");
-        System.out.println((amount) + "(руб) стоимость билета,");
-        System.out.println((bonusMile) + "(руб) стоимость одной бонусной милли?");
-        System.out.println("Решив простую алогарефмическую задачу мы получаем");
-        System.out.println((numberMile) + "боннусных милль");
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
-
